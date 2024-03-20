4efa84c35b496bca7c9f20521b983466af09d14b
Luqmaan Haffejee <luqhaf5@gmail.com>
2024-03-20 11:17:12 +0200

    initial components and routing

--- chorehive.client/src/app/app.routes.ts
+++ chorehive.client/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./ui/home/home.component";
+import { TaskOverviewComponent } from "./ui/task-overview/task-overview.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'tasks',
+    pathMatch: 'full',
+    component: TaskOverviewComponent
+  }
+];
